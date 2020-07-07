097fbcf90abc0a695ba185e2afa1fb20844f78d8
Amrendra Kumar <amarmca51@gmail.com>
2020-07-07 06:45:03 +0530

    added state

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
 import React from 'react';
 
 import './App.css';
-import {Provider}  from 'react-redux';
-import { createStore } from "redux";
-import Parent from "./Parent";
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import Parent from './Parent';
 
 //Initial State create
 const initialState = {
-  count: 0
+  count: 0,
 };
 
 //create reducer
-function reducer(state=initialState, action){
+function reducer(state = initialState, action) {
   switch (action.type) {
-    case "INCREMENT":
-      return {count : state.count +1}
+    case 'INCREMENT':
+      return {
+        ...state,
+        count: state.count + 1,
+      };
     default:
-    return {
-    count:state.count
-  }
+      return {
+        count: state.count,
+      };
   }
 }
 
 //Create store
-const store = createStore(reducer)
-console.log(store.getState())
+const store = createStore(reducer);
+console.log(store.getState());
 
 function App() {
   return (
-    <div className="App">
-    my App
-    <Provider store={store}>
-    <Parent />
-     </Provider>
+    <div className='App'>
+      my App
+      <Provider store={store}>
+        <Parent />
+      </Provider>
     </div>
   );
 }
